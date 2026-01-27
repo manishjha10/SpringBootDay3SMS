ec167b13637e39dd9199235e8f4e2dddda39bb15
Manish Jha <jhas67761@gmail.com>
2026-01-27 15:37:10 +0530

    added

--- src/main/java/com/example/Day3SMS/controller/StudentController.java
+++ src/main/java/com/example/Day3SMS/controller/StudentController.java
@@ -1,13 +1,42 @@
 package com.example.Day3SMS.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.Day3SMS.model.StudentModel;
+import com.example.Day3SMS.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+
 public class StudentController {
 
-    @GetMapping("/")
-    public String student(){
-        return "Hemepage for student";
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
     }
+
+//    create function API
+    @PostMapping("/add-student")
+    public StudentModel addStudent(@RequestBody StudentModel student) {
+        return service.addStudent(student);
+    }
+
+    @GetMapping("/students")
+    public List<StudentModel> getStudent(){
+        return service.getStudent();
+    }
+    @PutMapping("/update/{id}")
+    public StudentModel updateStudent(@PathVariable String id, @RequestBody StudentModel student){
+        return service.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteStudent(@PathVariable String id) {
+        service.deleteStudent(id);
+    }
+
+
+
+
 }
